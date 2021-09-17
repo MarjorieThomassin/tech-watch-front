4c9a1c37686a32bcb01c733614876f772a3ac741
MarjorieThomassin <marjorie.thomassin5@gmail.com>
2021-09-17 12:45:43 +0200

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+// eslint-disable-next-line no-unused-vars
+import App from "./App";
 
-test('renders learn react link', () => {
+// eslint-disable-next-line no-undef
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  // eslint-disable-next-line no-undef
   expect(linkElement).toBeInTheDocument();
 });
