5b0919bea1c54bdfff0857ca5010ac86dfed08fd
MarjorieThomassin <marjorie.thomassin5@gmail.com>
2021-09-22 18:46:17 +0200

    created comment input, addComment function and link it with resources

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-// eslint-disable-next-line no-unused-vars
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-// eslint-disable-next-line no-unused-vars
 import SearchBar from "./components/SearchBar";
-// eslint-disable-next-line no-unused-vars
+import Resource from "./components/Resource";
 import { ResourcesContextProvider } from "./contexts/ResourcesContext";
-import Resource from "./components/Resources";
-// eslint-disable-next-line no-unused-vars
 
 function App() {
   return (
